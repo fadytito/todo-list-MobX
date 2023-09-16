fcdcbeba3a117a162ec5f0f14227c14420eb6998
fady tito <fady.tito@vodafone.com>
2023-09-16 14:36:13 +0300

    initial setup

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import { Button, Input, Grid } from "@chakra-ui/react";
 
 function TodoAdd() {
+  const [inputVal, setInputVal] = React.useState('')
   return (
     <Grid pt={2} templateColumns="5fr 1fr" columnGap="3">
-      <Input placeholder="New todo" />
-      <Button>Add Todo</Button>
+      <Input placeholder="New todo" value={inputVal} onChange={(e) => setInputVal(e.target.value)}/>
+      <Button disabled={!!!inputVal}>Add Todo</Button>
     </Grid>
   );
 }
